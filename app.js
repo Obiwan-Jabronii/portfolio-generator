eca9a532f4011942d9d115b078d09234b0a10d8f
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-07-01 15:07:14 -0400

    installed inquirer and began prompts

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
-const fs = require('fs');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+import inquirer from 'inquirer';
 
-//   console.log('================');
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
+// const fs = require('fs');
+// const generatePage = require('./src/page-template')
 
-// printProfileData(profileDataArgs);
-const generatePage = (userName, githubName) => {
-    return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+// const pageHTML = generatePage(name, github)
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+
+
+// // const printProfileData = profileDataArr => {
+// //   // This...
+// //   for (let i = 0; i < profileDataArr.length; i += 1) {
+// //     console.log(profileDataArr[i]);
+// //   }
+
+// //   console.log('================');
+
+// //   // Is the same as this...
+// //   profileDataArr.forEach(profileItem => console.log(profileItem));
+// // };
+
+// // printProfileData(profileDataArgs);
+
+
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
