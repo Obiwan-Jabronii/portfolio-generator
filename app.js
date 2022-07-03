48c9ec89766514504b0a72dbb7de9ee83d4ee8d9
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-07-03 14:40:01 -0400

    updated the template to sort the data from app and assign its correct desigantion

--- app.js
+++ app.js
@@ -1,33 +1,9 @@
 import inquirer from 'inquirer';
 
+import fs from 'fs' 
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template')
+import generatePage from './src/page-template.js'
 
-// const pageHTML = generatePage(name, github)
-
-
-
-// // const printProfileData = profileDataArr => {
-// //   // This...
-// //   for (let i = 0; i < profileDataArr.length; i += 1) {
-// //     console.log(profileDataArr[i]);
-// //   }
-
-// //   console.log('================');
-
-// //   // Is the same as this...
-// //   profileDataArr.forEach(profileItem => console.log(profileItem));
-// // };
-
-// // printProfileData(profileDataArgs);
-
-
-// fs.writeFile('index.html', pageHTML, err => {
-//     if (err) throw err;
-  
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-//   });
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -127,5 +103,11 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-  console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
